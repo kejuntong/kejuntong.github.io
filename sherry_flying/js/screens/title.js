0cd26e2c2a924ffd633ecf16d55f7950f993fa0b
Kevin Tong <kejuntong@gmail.com>
2017-01-24 22:43:34 -0500

    ttt

--- sherry_flying/js/screens/title.js
+++ sherry_flying/js/screens/title.js
@@ -47,7 +47,7 @@ game.TitleScreen = me.ScreenObject.extend({
                 // size does not matter, it's just to avoid having a zero size
                 // renderable
                 this._super(me.Renderable, 'init', [0, 0, 100, 100]);
-                this.text = me.device.touch ? 'Tap to start' : 'PRESS SPACE OR CLICK LEFT MOUSE BUTTON TO START \n\t\t\t\t\t\t\t\t\t\t\tPRESS "M" TO MUTE SOUND';
+                this.text = me.device.touch ? '点一下开始' : '按空格开始 \n\t\t\t\t\t\t\t\t\t\t\t笑死我了哈哈哈哈哈';
                 this.font = new me.Font('gamefont', 20, '#000');
             },
             draw: function (renderer) {
